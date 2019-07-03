1d3261833485639df9a88d6620b35a458927bd0e
oneraul <oneraul@gmail.com>
2019-07-04 01:58:08 +0200

    Fixed typo

--- Common/src/physics/ForceEmitter.h
+++ Common/src/physics/ForceEmitter.h
@@ -8,7 +8,7 @@ namespace rmkl {
 	class ForceEmitter
 	{
 	public:
-		virtual bool Update(int tick) = 0; // returns true if it wants to se a new snapshot
+		virtual bool Update(int tick) = 0; // returns true if it wants to set a new snapshot
 		virtual void ApplyForce(Rigidbody& body) = 0;
 
 		virtual Aabb GetDebugAabb(int tick) = 0;
