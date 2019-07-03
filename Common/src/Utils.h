67d2853801587db8df84ea95db56d4bdc8bcb4bf
oneraul <oneraul@gmail.com>
2019-07-03 21:16:26 +0200

    Removed the template functions RemoveOldSnapshots from Utils

--- Common/src/Utils.h
+++ Common/src/Utils.h
@@ -14,27 +14,5 @@ namespace utils {
 
 	template <class T>
 	inline int Sign(T value) { return (value > 0) ? 1 : ((value < 0) ? -1 : 0); }
-
-	template<typename T>
-	void RemoveOldSnapshots(std::vector<T>& buffer, int tick)
-	{
-		buffer.erase(std::remove_if(buffer.begin(), buffer.end(), 
-			[=](const T& snapshot) -> bool { return snapshot.Tick <= tick; }), 
-			buffer.end());
-	}
-
-	template<typename T>
-	void RemoveOldSnapshots(std::map<int, T>& buffer, int tick)
-	{
-		std::map<int, T>::iterator it = buffer.begin();
-		while (it != buffer.end())
-		{
-			if (it->first <= tick)
-				it = buffer.erase(it);
-			else
-				it++;
-		}
-	}
-
 }
 }
